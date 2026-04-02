e286cbf6f7e243cb07bf0403d4aba8f2a03b0f39
Jeong Jihun <doksakim7@naver.com>
2026-04-02 15:16:15 +0900

    refactor: 레벨1 5번 주석 추가

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -18,6 +18,7 @@ public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
 
+    // ✅ 레벨1 5번 After -> Before 수정, 괄호 안 클래스의 위치도 요구사항에 맞게 변경
     @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
     public void logBeforeChangeUserRole(JoinPoint joinPoint) {
         String userId = String.valueOf(request.getAttribute("userId"));
